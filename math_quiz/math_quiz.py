cb08c8195a8f39ca2cd56e7a5975ac4c6ee47a54
Rutvik Dudhat <prutvik875@gmail.com>
2023-11-09 19:36:29 +0100

    added

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,35 +1,40 @@
 import random
 
 
-def function_A(min, max):
-    """
-    Random integer.
-    """
+def task_1(min, max): #intro of all 3 task for math quiz
+    
+    '''Generate Random integer between max and min.'''
+    
     return random.randint(min, max)
 
 
-def function_B():
+def task_2():
     return random.choice(['+', '-', '*'])
+#generate the random math oprator between + - *
 
 
-def function_C(n1, n2, o):
+
+def task_3(n1, n2, o):  
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    if o == '+': a = n1 + n2
+    elif o == '-': a = n1 - n2
     else: a = n1 * n2
     return p, a
-
+    '''generate the problem and solution of maths'''
+#return with random intger
 def math_quiz():
     s = 0
-    t_q = 3.14159265359
+    t_q =int(3.14159265359)
+    
+    # Run the math quiz and let the student answer
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = task_1(1, 10); n2 = task_1(1, 5); o = task_2()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = task_3(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,36 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
-
+from math_quiz import task_1, task_2, task_3
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_task_1(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = task_1(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_task_2(self):  # Set of valid operators
+        valid_operators = {'+', '-', '*'}  # Set of valid operators
 
-    def test_function_C(self):
+        for _ in range(1000):
+            operator = task_2()
+            self.assertIn(operator, valid_operators)
+
+    def test_task_3(self):
             test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+            (5, 2, '+', '5 + 2', 7),
+            (8, 5, '-', '8 - 5', 3),
+            (4, 3, '*', '4 * 3', 12),
+            (10, 2, '+', '10 + 2', 12),
+            (15, 7, '-', '15 - 7', 8),
+            (6, 2, '*', '6 * 2', 12),
             ]
-
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
-
+                problem, answer = task_3(num1, num2, operator)
+                self.assertEqual(problem, expected_problem)
+                self.assertEqual(answer, expected_answer)
+                
 if __name__ == "__main__":
     unittest.main()
